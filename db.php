c3053437badb25a4d040417aea4d47e39e9b84ba
irene50 <60002985+irene50@users.noreply.github.com>
2020-04-13 14:55:00 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,12 @@
 <?php
    /*define('DB_SERVER', 'localhost');*/
    define('DB_SERVER', '10.130.14.191'); /*esto pá que se conecte al openshift*/
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'rootroot');
-   define('DB_DATABASE', 'pekipet');
+   /*define('DB_USERNAME', 'root');*/
+   define('DB_USERNAME', 'pekipet');
+   /*define('DB_PASSWORD', 'rootroot');*/
+   define('DB_PASSWORD', 'pekipet');
+   /*define('DB_DATABASE', 'pekipet');*/
+   define('DB_DATABASE', 'dbPekipet');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    if (!$db) {
 		die("Error conexión: " . mysqli_connect_error());
