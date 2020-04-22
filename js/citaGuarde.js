0751e8a911df8e324e3e22af40517fd1b3de641f
pekipet <62712781+pekipet@users.noreply.github.com>
2020-04-22 14:28:50 +0200

    Add files via upload
    Preparando para la tutoria

---
+++ js/citaGuarde.js
@@ -0,0 +1,133 @@
+window.onload = function() {
+    //document.primero.nombre.readOnly = true;
+    //document.primero.apellidos.readOnly = true;
+    //document.primero.email.readOnly = true;
+    document.primero.precio.readOnly = true;
+    //document.primero.onsubmit = enviar;
+}
+
+if (document.addEventListener) {
+	window.addEventListener('load', iniciar);
+} else if (document.attachEvent) {
+	window.attachEvent('onload', iniciar);
+}
+
+var varSelect1_1 = new Array('Jornada completa mensual', 'Media jornada mensual', 'Media jornada');
+var varSelect1_2 = new Array('1 hora', '2 horas', '3 horas', '4 horas', '5 horas');
+
+function iniciar() {
+	if (document.addEventListener) {
+        document.getElementById('form-tiempo').addEventListener('change', citas);
+        document.getElementById('formtiempo2').addEventListener('change', precio);
+        document.getElementById('form-tiempo').addEventListener('change', precio2);
+        document.getElementById('cita').addEventListener('submit', enviar);
+        
+	} else if (document.attachEvent) {
+        document.getElementById('form-tiempo').attachEvent('onchange', citas);
+        document.getElementById('formtiempo2').attachEvent('onchange', precio);
+        document.getElementById('form-tiempo').attachEvent('onchange', precio2);
+        document.getElementById('cita').attachEvent('onsubmit', enviar);
+	}
+}
+
+function citas() {
+    var valor = document.getElementById('form-tiempo').value;
+   	if (valor != 0) { 
+      	var varSelect1 = eval("varSelect1_" + valor);
+      	var vnbSelect1 = varSelect1.length;
+      	document.primero.formtiempo2.length = vnbSelect1;
+      	for(i = 0; i < vnbSelect1; i++){ 
+         	document.getElementById('formtiempo2').options[i].text = varSelect1[i];
+      	}	
+   	}else{ 
+      	document.getElementById('formtiempo2').length = 1;
+        document.getElementById('formtiempo2').options[0].text = "-";
+        document.getElementById('formprecio').value = 0;
+   	} 
+       document.getElementById('formtiempo2').options[0].selected = true;
+}
+
+function precio() {
+    var valor = document.getElementById('formtiempo2').value;
+    switch (valor) {
+        case 'Jornada completa mensual':
+            document.getElementById('formprecio').value = 300;
+          break;
+        case 'Media jornada mensual':
+            document.getElementById('formprecio').value = 200;
+            break;
+        case 'Media jornada':
+            document.getElementById('formprecio').value = 16;
+          break;
+        case '1 hora':
+            document.getElementById('formprecio').value = 6;
+            break;
+        case '2 horas':
+            document.getElementById('formprecio').value = 8;
+            break;
+        case '3 horas':
+            document.getElementById('formprecio').value = 10;
+            break;
+        case '4 horas':
+            document.getElementById('formprecio').value = 12;
+            break;
+        case '5 horas':
+            document.getElementById('formprecio').value = 14;
+            break;
+        default:
+            document.getElementById('formprecio').value = 0;
+          break;
+      }
+}
+
+function precio2() {
+    var valor = document.getElementById('form-tiempo').value;
+    if (valor == 1) {
+        document.getElementById('formprecio').value = 300;
+    } else if (valor == 2) {
+        document.getElementById('formprecio').value = 6;
+    } else {
+        document.getElementById('formprecio').value = 0;
+    }
+    
+    
+}
+
+function enviar() {
+    var vblEnviar = true;
+    var vNombre = document.primero.nombre.value.trim();
+    var vApellidos = document.primero.apellidos.value.trim();
+    var vEmail = document.primero.email.value.trim();
+    var vTelefono = document.primero.telefono.value.trim();
+    
+    var verNombre = /^[a-z]([a-z]|\s|\-)+[a-z]$/i;
+    var verApellidos = /^[a-z]([a-z]|\s|\-)+[a-z]$/i;
+    var verEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.[a-z]{2,4})+$/i;
+    var verTelefono = /^[679]\d{8}$/;
+
+    if (!verNombre.test(vNombre)){
+        vblEnviar = false;
+        document.primero.nombre.value = "";
+		document.primero.nombre.placeholder = "ERROR!";
+		document.primero.nombre.focus();
+	}
+    if (!verApellidos.test(vApellidos)) {
+        vblEnviar = false;
+        document.primero.apellidos.value = "";
+        document.primero.apellidos.placeholder = "ERROR!";
+		document.primero.apellidos.focus();
+    }
+    if (!verEmail.test(vEmail)) {
+        vblEnviar = false;
+        document.primero.email.value = "";
+        document.primero.email.placeholder = "ERROR!";
+		document.primero.email.focus();
+    }
+    if (!verTelefono.test(vTelefono)) {
+        vblEnviar = false;
+        document.primero.telefono.value = "";
+        document.primero.telefono.placeholder = "ERROR!";
+		document.primero.telefono.focus();
+    }
+    return vblEnviar;
+}
---
+++ js/validaciones.js
@@ -0,0 +1,69 @@
+window.onload = function() {
+    document.primero.onsubmit = enviar;
+}
+
+function enviar() {
+    var vblEnviar = true;
+    var vNombre = document.primero.nombre.value.trim();
+    var vApellido1 = document.primero.apellido1.value.trim();
+    var vApellido2 = document.primero.apellido2.value.trim();
+    var vUsuario = document.primero.usuario.value.trim();
+    var vContrasena = document.primero.contrasena.value.trim();
+    var vEmail = document.primero.email.value.trim();
+    var vTelefono = document.primero.telefono.value.trim();
+    
+    var verNombre = /^[a-z]([a-z]|\s|\-)+[a-z]$/i;
+    var verApellido = /^[a-z]([a-z]|\s|\-)+[a-z]$/i;
+    var verUsuario = /^[a-z](\w|\_)+[\w|\.]$/i;
+    var verContrasena = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)([a-zA-Z\d@$!%*?&]|[^ ]){8,15}$/;
+    var verEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.[a-z]{2,4})+$/i;
+    var verTelefono = /^[679]\d{8}$/;
+    
+    if (!verNombre.test(vNombre)){
+        vblEnviar = false;
+        document.primero.nombre.value = "";
+		document.primero.nombre.placeholder = "ERROR!";
+		document.primero.nombre.focus();
+	}
+    if (!verApellido.test(vApellido1)) {
+        vblEnviar = false;
+        document.primero.apellido1.value = "";
+        document.primero.apellido1.placeholder = "ERROR!";
+		document.primero.apellido1.focus();
+    }
+    if (!verApellido.test(vApellido2) && vApellido2 != "") {
+        vblEnviar = false;
+        document.primero.apellido2.value = "";
+        document.primero.apellido2.placeholder = "ERROR!";
+		document.primero.apellido2.focus();
+    }
+    if (!verUsuario.test(vUsuario)) {
+        vblEnviar = false;
+        document.primero.usuario.value = "";
+        document.primero.usuario.placeholder = "ERROR!";
+		document.primero.usuario.focus();
+    }
+    if (!verContrasena.test(vContrasena)) {
+        vblEnviar = false;
+        document.primero.contrasena.value = "";
+        document.primero.contrasena.placeholder = "Al menos mayuscula, minuscula y digito. Entre 8 y 15.";
+		document.primero.contrasena.focus();
+    }
+    if (!verEmail.test(vEmail)) {
+        vblEnviar = false;
+        document.primero.email.value = "";
+        document.primero.email.placeholder = "ERROR!";
+		document.primero.email.focus();
+    }
+    if (!verTelefono.test(vTelefono) && vTelefono != "") {
+        vblEnviar = false;
+        document.primero.telefono.value = "";
+        document.primero.telefono.placeholder = "ERROR!";
+		document.primero.telefono.focus();
+    }
+    if (vblEnviar) {
+        //alert("Formulario enviado");
+        msj('Login no válido','Tu usuario o contraseña son incorrectos','Volver');
+    }
+    return vblEnviar;
+}
