4a7da6c5657aef2f872a29a76fcb30d1a7b90363
pekipet <62712781+pekipet@users.noreply.github.com>
2020-05-10 14:42:05 +0200

    Add files via upload

--- includes/js/validaciones2.js
+++ includes/js/validaciones2.js
@@ -1,22 +1,22 @@
-window.onload = function() {
-    document.contacto.onsubmit = enviar;
-}
+window.onload = function () {
+  document.contacto.onsubmit = enviar;
+};
 
 function enviar() {
-    var vblEnviar = true;
-    var vEmail = document.contacto.email.value.trim();
-    console.log(vEmail);
-    var verEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.[a-z]{2,4})+$/i;
+  var vblEnviar = true;
+  var vEmail = document.contacto.email.value.trim();
+  console.log(vEmail);
+  var verEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.[a-z]{2,4})+$/i;
 
-    if (!verEmail.test(vEmail)) {
-        vblEnviar = false;
-        document.contacto.email.value = "";
-        document.contacto.email.placeholder = "ERROR!";
-		document.contacto.email.focus();
-    }
-    if (vblEnviar) {
-        //alert("Formulario enviado");
-        msj('Login no válido','Tu usuario o contraseña son incorrectos','Volver');
-    }
-    return vblEnviar;
+  if (!verEmail.test(vEmail)) {
+    vblEnviar = false;
+    document.contacto.email.value = "";
+    document.contacto.email.placeholder = "ERROR!";
+    document.contacto.email.focus();
+  }
+  if (vblEnviar) {
+    //alert("Formulario enviado");
+    //msj('Login no válido','Tu usuario o contraseña son incorrectos','Volver');
+  }
+  return vblEnviar;
 }
