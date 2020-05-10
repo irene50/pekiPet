45ea276d49f87b3312939fd553c0a2026e00b620
pekipet <62712781+pekipet@users.noreply.github.com>
2020-05-10 14:56:31 +0200

    Add files via upload

--- entra.php
+++ entra.php
@@ -11,45 +11,45 @@ include_once 'header.php';
     <div class=" col-lg-5 mb-4 formuEntra">
 
       <!--Form with header-->
-      <form name="primero" action="loguear.php" method = "post">
-      <div class="card formu">
+      <form name="primero" action="loguear.php" method="post">
+        <div class="card formu">
 
-        <div class="card-body ">
-          <!--Header-->
-          <div class="form-header blue accent-1">
-            <h3 align="center"><i class="fa fa-paw"></i> Log in: <i class="fa fa-paw"></i></h3>
-          </div>
-          <br>
+          <div class="card-body ">
+            <!--Header-->
+            <div class="form-header blue accent-1">
+              <h3 align="center"><i class="fa fa-paw"></i> Entrar: <i class="fa fa-paw"></i></h3>
+            </div>
+            <br>
 
-          <!--Body-->
-          <div class="md-form">
-            <label for="form-user"><i class="fa fa-user prefix grey-text"></i> Usuario </label>
-            <input type="text" name="user" id="form-user" class="form-control">
+            <!--Body-->
+            <div class="md-form">
+              <label for="form-user"><i class="fa fa-user prefix grey-text"></i> Usuario </label>
+              <input type="text" name="user" id="form-user" class="form-control">
+            </div>
+            <div class="md-form">
+              <label for="form-password"> <i class="fa fa-key prefix grey-text"></i> Contraseña </label>
+              <input type="password" name="password" id="form-password" class="form-control">
+            </div>
+            <a href="./recordar.php">
+              <div id="pekipet">¿Olvidaste tu usuario o contraseña?</div>
+            </a>
+            <div class="text-center mt-4">
+              <button name="entra" class="btn btn-light-blue" style="background-color: aquamarine;">Entra</button>
+            </div>
+            <div class="text-center mt-4">
+              <p>¿Aún no estás registrado? ¿A que esperas?</p>
+              <a href="./registrate.php"><button name="regis" class="btn btn-light-blue" style="background-color: red;">Regístrate</button></a>
+            </div>
           </div>
-          <div class="md-form">
-            <label for="form-password"> <i class="fa fa-key prefix grey-text"></i> Contraseña </label>
-            <input type="password" name="password" id="form-password" class="form-control">
-          </div>
-          <a href="./recordar.php">
-            <div id="pekipet">Olvidaste tu usuario o contraseña?</div>
-          </a>
-          <div class="text-center mt-4">
-            <button name="entra" class="btn btn-light-blue" style="background-color: aquamarine;">Entra</button>
-          </div>
-          <div class="text-center mt-4">
-            <p>Aún no estás registrado? A que esperas?</p>
-            <a href="./registrate.php"><button name="regis" class="btn btn-light-blue" style="background-color: red;">Regístrate</button></a>
-          </div>
+
         </div>
-
-      </div>
       </form>
       <!--Form with header-->
 
     </div>
     <!--Grid column-->
   </div>
-  </section>
+</section>
 <?php
 include_once 'footer.php';
 ?>
--- index.php
+++ index.php
@@ -4,51 +4,51 @@ include_once 'header.php';
 <script src="includes/js/validaciones2.js"></script>
 <div class="wrapper">
 
-<ul id="sb-slider" class="sb-slider">
-  <li>
-    <a href="indexInfoGuarde.php" target="_blank"><img src="includes/imagenes/guarderia1.jpg" alt="image1"/></a>
-    <div class="sb-description">
-      <h3>Cuidamos a tus animales</h3>
-    </div>
-  </li>
-  <li>
-    <a href="indexInfoGuarde.php" target="_blank"><img src="includes/imagenes/2.jpg" alt="image2"/></a>
-    <div class="sb-description">
-      <h3>Estarán encantados!</h3>
-    </div>
-  </li>
-  <li>
-    <a href="indexInfoGuarde.php" target="_blank"><img src="includes/imagenes/3.jpg" alt="image3"/></a>
-    <div class="sb-description">
-      <h3>En Peki-Pet estarán en buenas manos</h3>
-    </div>
-  </li>
-  <li>
-    <a href="indexInfoGuarde.php" target="_blank"><img src="includes/imagenes/4.jpg" alt="image4"/></a>
-    <div class="sb-description">
-      <h3>Lucirán fabulosos!</h3>
-    </div>
-  </li>
-</ul>
+  <ul id="sb-slider" class="sb-slider">
+    <li>
+      <a href="indexInfoGuarde.php" target="_blank"><img src="includes/imagenes/perro22.jpg" alt="image1" /></a>
+      <div class="sb-description">
+        <h3>Cuidamos a tus animales</h3>
+      </div>
+    </li>
+    <li>
+      <a href="indexInfoGuarde.php" target="_blank"><img src="includes/imagenes/perro11.jpg" alt="image2" /></a>
+      <div class="sb-description">
+        <h3>¡Estarán encantados!</h3>
+      </div>
+    </li>
+    <li>
+      <a href="indexInfoGuarde.php" target="_blank"><img src="includes/imagenes/perro33.jpg" alt="image3" /></a>
+      <div class="sb-description">
+        <h3>En Peki-Pet estarán en buenas manos</h3>
+      </div>
+    </li>
+    <li>
+      <a href="indexInfoGuarde.php" target="_blank"><img src="includes/imagenes/perro44.jpg" alt="image4" /></a>
+      <div class="sb-description">
+        <h3>¡Harán amistades!</h3>
+      </div>
+    </li>
+  </ul>
 
-<div id="sombra" class="sombra"></div>
+  <div id="sombra" class="sombra"></div>
 
-<div id="nav-arrows" class="nav-arrows">
-  <a href="#">Next</a>
-  <a href="#">Previous</a>
-</div>
-<!-- Opcion 1 -->
-<div id="nav-dots" class="nav-dots">
-					<span class="nav-dot-current"></span>
-					<span></span>
-					<span></span>
-					<span></span>
-        </div>
-<!-- Opcion 2 -->
-<div id="nav-options" class="nav-options">
-  <span id="navPlay">Play</span>
-  <span id="navPause">Pause</span>
-</div>
+  <div id="nav-arrows" class="nav-arrows">
+    <a href="#">Next</a>
+    <a href="#">Previous</a>
+  </div>
+  <!-- Opcion 1 -->
+  <div id="nav-dots" class="nav-dots">
+    <span class="nav-dot-current"></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+  <!-- Opcion 2 -->
+  <div id="nav-options" class="nav-options">
+    <span id="navPlay">Play</span>
+    <span id="navPause">Pause</span>
+  </div>
 </div><!-- /wrapper -->
 <!-- Flexslider viejo 
 <div class="flexslider" id="slider">
@@ -79,21 +79,23 @@ include_once 'header.php';
     </li>
   </ul>
 </div>-->
-<div><h1 class="titulo" id="servicios" align="center">Servicios</h1></div>
+<div>
+  <h1 class="titulo" id="servicios" align="center">Servicios</h1>
+</div>
 <div class="card-deck">
   <div class="card">
-    
+
     <div class="card-body guarde">
       <h4 class="card-title" align="center">Guardería canina</h4>
       <p class="card-text">Nuestra guardería canina no te dejará indiferente. Los peludos se lo pasan genial, realizando juegos interactivos, ejercitando la mente y cuerpo.
-        Damos largos paseos por el parque, disfrutan de otros compañeros perrunos y sobretodo reciben mucho mucho amor para que no echen de menos a sus papis... <a id="masInfo" href="indexInfoGuarde.php"><i class="fa fa-info" aria-hidden="true"></i>  Más información</a></a></p>
+        Damos largos paseos por el parque, disfrutan de otros compañeros perrunos y sobretodo reciben mucho mucho amor para que no echen de menos a sus papis... <a id="masInfo" href="indexInfoGuarde.php"><i class="fa fa-info" aria-hidden="true"></i> Más información</a></a></p>
     </div>
     <div class="card-footer">
       <small class="pie reserva"><a href="formuGuarde.php">Reserva online</a></small>
     </div>
   </div>
   <div class="card">
-    
+
     <div class="card-body pelu">
       <h4 class="card-title" align="center">Peluquería canina</h4>
       <p class="card-text">No dejes de poner guapo a tu peludito con nosotros. ¡Te aseguramos que será la/el más coqueto del barrio!</p>
@@ -102,13 +104,15 @@ include_once 'header.php';
       <small class="pie cita"><a href="formuPelu.php">Cita online</a></small>
     </div>
   </div>
- 
-  </div>
+
 </div>
-<div><h1 class="titulo" id="tarifas" align="center">Tarifas</h1></div>
+</div>
+<div>
+  <h1 class="titulo" id="tarifas" align="center">Tarifas</h1>
+</div>
 <!--TARIFAS-->
-<div class="textoT" align="center"><span class="textoTarifa" >¡Nuestras tarifas son inmejorables! Consúltalas y cualquier duda o sugerencia, contáctanos</span></div>
-<div class="botonT" align="center"><input type="button" id="botonTarifa" value="Consultar tarifas" ></div>
+<div class="textoT" align="center"><span class="textoTarifa">¡Nuestras tarifas son inmejorables! Consúltalas y cualquier duda o sugerencia, contáctanos</span></div>
+<div class="botonT" align="center"><input type="button" id="botonTarifa" value="Consultar tarifas"></div>
 <div id="bloqueTarifas" class="container tarifas oculto" style="margin-top:30px;">
   <div class="col-md-6 precios">
     <h3 align="center">Tarifas guardería</h3>
@@ -158,7 +162,9 @@ include_once 'header.php';
           </ul>
         </div>
       </div>
-      <div><h6> * Precios orientativos a especificar por la peluquera</h6></div>
+      <div>
+        <h6> * Precios orientativos a especificar por la peluquera</h6>
+      </div>
     </div>
   </div>
 </div>
@@ -191,8 +197,10 @@ include_once 'header.php';
   </div>
 </div>-->
 <!--TARIFAS-->
-<div><h1 class="titulo" id="contacto" align="center">Contacto</h1></div>
-  <!--Section: Contact v.1-->
+<div>
+  <h1 class="titulo" id="contacto" align="center">Contacto</h1>
+</div>
+<!--Section: Contact v.1-->
 <section class="section pb-5">
 
   <div class="row">
@@ -201,51 +209,51 @@ include_once 'header.php';
     <div class="col-lg-5 mb-4">
 
       <!--Form with header-->
-    <form action="sendContacto.php" method="POST" name="contacto" id ="contacto">
-      <div class="card formu">
+      <form action="sendContacto.php" method="POST" name="contacto" id="contacto">
+        <div class="card formu">
 
-        <div class="card-body ">
-          <!--Header-->
-          <div class="form-header blue accent-1">
-            <h3><i class="fa fa-envelope-o"></i> Escríbenos:</h3>
-          </div>
+          <div class="card-body ">
+            <!--Header-->
+            <div class="form-header blue accent-1">
+              <h3><i class="fa fa-envelope-o"></i> Escríbenos:</h3>
+            </div>
 
-          <p>Te contestaremos con la mayor brevedad posible.</p>
-          <br>
+            <p>Te contestaremos con la mayor brevedad posible.</p>
+            <br>
 
-          <!--Body-->
-          <div class="md-form">
-            <label for="form-name"><i class="fa fa-user prefix grey-text"></i> Tu nombre</label>
-            <input type="text" id="form-name" name="form-name" class="form-control">
-            
-          </div>
+            <!--Body-->
+            <div class="md-form">
+              <label for="form-name"><i class="fa fa-user prefix grey-text"></i> Tu nombre</label>
+              <input type="text" id="form-name" name="form-name" class="form-control">
 
-          <div class="md-form">
-            <label for="email"><i class="fa fa-envelope prefix grey-text"></i> Tu email</label>
-            <input type="email" id="email" name="email" required class="form-control">
-            
-          </div>
+            </div>
 
-          <div class="md-form">
-            <label for="form-Subject"><i class="fa fa-tag prefix grey-text"></i> Asunto</label>
-            <input type="text" id="form-Subject" name="form-Subject" class="form-control">
-            
-          </div>
+            <div class="md-form">
+              <label for="email"><i class="fa fa-envelope prefix grey-text"></i> Tu email</label>
+              <input type="email" id="email" name="email" required class="form-control">
 
-          <div class="md-form">
-            <label for="form-text"><i class="fa fa-pencil prefix grey-text"></i> Comentario</label>
-            <textarea id="form-text" name="form-text" class="form-control md-textarea" rows="3"></textarea>
-            
-          </div>
+            </div>
 
-          <div class="text-center mt-4">
-            <input type="submit" class="btn btn-light-blue" style="background-color: cadetblue;">
+            <div class="md-form">
+              <label for="form-Subject"><i class="fa fa-tag prefix grey-text"></i> Asunto</label>
+              <input type="text" id="form-Subject" name="form-Subject" class="form-control">
+
+            </div>
+
+            <div class="md-form">
+              <label for="form-text"><i class="fa fa-pencil prefix grey-text"></i> Comentario</label>
+              <textarea id="form-text" name="form-text" class="form-control md-textarea" rows="3"></textarea>
+
+            </div>
+
+            <div class="text-center mt-4">
+              <input type="submit" class="btn btn-light-blue" style="background-color: cadetblue;">
+            </div>
+
           </div>
 
         </div>
-
-      </div>
-    </form>
+      </form>
       <!--Form with header-->
 
     </div>
@@ -255,12 +263,11 @@ include_once 'header.php';
     <div class="col-lg-7">
 
       <!--Google map-->
-      <div id="map-container-google-11" class="z-depth-1-half map-container-6" >
-        <iframe src="https://maps.google.com/maps?q=madrid&t=&z=13&ie=UTF8&iwloc=&output=embed"
-          frameborder="0" style="border:0; height: 90%;" allowfullscreen></iframe>
+      <div id="map-container-google-11" class="z-depth-1-half map-container-6">
+        <iframe src="https://maps.google.com/maps?q=madrid&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" style="border:0; height: 90%;" allowfullscreen></iframe>
       </div>
 
-      
+
       <!--Buttons-->
       <div class="row text-center">
         <div class="col-md-4 contactIcon">
