4d66649dc2ea4e57d9c8fe011795cb7c7e1c90dd
pekipet <62712781+pekipet@users.noreply.github.com>
2020-06-07 12:48:24 +0200

    Update db.php

--- db.php
+++ db.php
@@ -5,7 +5,7 @@
    define('DB_DATABASE', 'pekipet');
 
    esto pá que se conecte al openshift*/
-   define('DB_SERVER', '10.129.24.29:3306');
+   define('DB_SERVER', '10.129.14.16:3306');
    define('DB_USERNAME', 'pekipet');
    define('DB_PASSWORD', 'pekipet');
    define('DB_DATABASE', 'pekipetdb');
